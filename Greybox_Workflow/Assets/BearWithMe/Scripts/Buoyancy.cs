fcd74f6d99e187698fe59fe53e8782f62b7b86ea
Steadz <markelvisphillips@gmail.com>
2017-10-02 15:31:45 +1100

    Added Platform script and sinking

---
+++ Greybox_Workflow/Assets/BearWithMe/Scripts/Buoyancy.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buoyancy : MonoBehaviour
+{
+    //-----------Serialize Fields-----------
+    [SerializeField]
+    private float buoyancyForce = 250.0f;
+    [SerializeField]
+    private float rotationCorrection = 5.0f;
+    [SerializeField]
+    private float waterLevel = 1.0f;
+    [SerializeField]
+    private float splashDownForce = -1.25f;
+
+    //-----------Floats------------
+    private float calculatedBuoyancy;
+    private float airResist;
+    private float waterResist;
+
+    //----------Booleans---------
+    private bool submerged;
+
+    //--------Vector 3's---------
+    private Vector3 angle;
+
+    private Rigidbody rb;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        airResist = rb.drag;
+        submerged = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+        ClampRotation();
+
+        if (transform.position.y > waterLevel)
+        {
+            submerged = false;
+        }
+
+        //move the object on Y towards water level
+        //calculate vector between water level and object position
+        Vector3 vecBetween = (new Vector3(transform.position.x, waterLevel + transform.localScale.y, transform.position.z) - transform.position);
+
+
+        //if object center is below the water
+        if ( transform.position.y < waterLevel)
+        {
+            //if you were out of water last frame
+            if (submerged == false)
+            {
+                //add impulse force
+                rb.AddForce(0, splashDownForce * rb.velocity.y, 0, ForceMode.Impulse);
+            }
+           
+            //calculate buoyancy based on the distance and a force
+            // calculatedBuoyancy = vecBetween.magnitude * buoyancyForce;
+            submerged = true;
+            waterResist = vecBetween.magnitude * 0.1f;
+            rb.drag = waterResist;
+            rb.AddForce(vecBetween * buoyancyForce * Time.fixedDeltaTime);
+        }
+        else
+        {
+            //no buoyancy in the air
+            rb.drag = airResist;
+        }
+
+        if (transform.rotation != Quaternion.identity)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, (rotationCorrection * Time.fixedDeltaTime));
+        }
+
+    }
+
+    //Clamps local rotation x and z of object
+    private void ClampRotation()
+    {
+        angle = transform.localRotation.eulerAngles;
+
+        if (angle.x < 355.0f && angle.x > 340.0f)
+        {
+            angle = transform.localRotation.eulerAngles;
+            angle.x = 355.0f;
+            transform.localRotation = Quaternion.Euler(angle);
+        }
+
+        if (angle.x > 5.0f && angle.x < 15.0f)
+        {
+            angle = transform.localRotation.eulerAngles;
+            angle.x = 5.0f;
+            transform.localRotation = Quaternion.Euler(angle);
+        }
+
+        if (angle.z < 355.0f && angle.z > 340.0f)
+        {
+            angle = transform.localRotation.eulerAngles;
+            angle.z = 355.0f;
+            transform.localRotation = Quaternion.Euler(angle);
+        }
+        if (angle.z > 5.0f && angle.z < 15.0f)
+        {
+            angle = transform.localRotation.eulerAngles;
+            angle.z = 5.0f;
+            transform.localRotation = Quaternion.Euler(angle);
+        }
+    }
+}
---
+++ Greybox_Workflow/Assets/BearWithMe/Scripts/Platform.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject plat1;
+    private GameObject plat2;
+    private GameObject plat3;
+    private GameObject plat4;
+    private int platformNum;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        plat1 = GameObject.Find("Plat1");
+        plat2 = GameObject.Find("Plat2");
+        plat3 = GameObject.Find("Plat3");
+        plat4 = GameObject.Find("Plat4");
+        //default value (value > 3 || < 0)
+        platformNum = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1")) //---This IF is For testing so it doesnt run on first frame of update
+        {
+            switch (platformNum)
+            {
+                case 0:
+                    {
+                        rb = plat1.GetComponent<Rigidbody>();
+                        rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+                        platformNum = -1;
+                        break;
+                    }
+                case 1:
+                    {
+                        rb = plat2.GetComponent<Rigidbody>();
+                        rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+                        platformNum = -1;
+                        break;
+                    }
+                case 2:
+                    {
+                        rb = plat3.GetComponent<Rigidbody>();
+                        rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+                        platformNum = -1;
+                        break;
+                    }
+                case 3:
+                    {
+                        rb = plat4.GetComponent<Rigidbody>();
+                        rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+                        platformNum = -1;
+                        break;
+                    }
+                default:
+                    {
+                        platformNum = -1;
+                        break;
+                    }
+            }
+        }
+
+        //------------------For Demo-------------------------
+        //if (Input.GetKeyDown("1"))
+        //{
+        //    rb = plat1.GetComponent<Rigidbody>();
+        //    rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        //}
+        //if (Input.GetKeyDown("2"))
+        //{
+        //    rb = plat2.GetComponent<Rigidbody>();
+        //    rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        //}
+        //if (Input.GetKeyDown("3"))
+        //{
+        //    rb = plat3.GetComponent<Rigidbody>();
+        //    rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        //}
+        //if (Input.GetKeyDown("4"))
+        //{
+        //    rb = plat4.GetComponent<Rigidbody>();
+        //    rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        //}
+
+    }
+}
+
+
