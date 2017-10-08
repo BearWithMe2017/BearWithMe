5ed544cfca90bc023c6de4863d86ea613a8cdc49
Steadz <markelvisphillips@gmail.com>
2017-10-09 10:02:22 +1100

    Gamemanger

---
+++ Greybox_Workflow/Assets/BearWithMe/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+
+public class GameManager : MonoBehaviour
+{
+	private void Awake()
+	{
+		//find any game manager
+		GameManager[] managers = FindObjectsOfType<GameManager>();
+		
+		if(managers.Length > 1)
+		{
+			//destroy yourself, there's already a game manager
+			GameObject.Destroy(gameObject);
+		}
+		else
+		{	
+			//set yourself to not destroy (because you are the gamemanager)
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+			
+	}
+}
