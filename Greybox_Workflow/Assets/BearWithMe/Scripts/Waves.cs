825bb41725b9e1132a7ba7521a72a694018898af
Steadz <markelvisphillips@gmail.com>
2017-11-14 15:52:38 +1100

    fake waves for cans

---
+++ Greybox_Workflow/Assets/BearWithMe/Scripts/Waves.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour {
+    [SerializeField] float speed;
+
+    // Use this for initialization
+    void Start ()
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //transform.position = new Vector3(transform.position.x, Mathf.Sin(Mathf.Lerp(-0.1f, 0.1f, Mathf.PingPong(Time.time * speed, 1.0f))), transform.position.z);
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time * speed) * 0.1f, transform.position.z);
+    }
+}
